87c4b632ff6d1feaaa39141143ea6e076bc149cf
Charles Fontaine <fontaine.charles64@gmail.com>
2026-05-30 16:00:52 +0200

    feat(scan): update route scan (post) to launch a runAllTests orchestrator and save the filtered results by rgaa categories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fetch = require("node-fetch");
+const runAllTests = require("../tests/runAllTests.js");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 0. route POST qui lance un audit et récupère la key url dans le corps de la requête
+router.post("/scan", async (req, res) => {
+  const { url } = req.body;
+
+  if (url === undefined || url === '') {
+    res.status(403).json({result: false, error: 'Missing or empty url'})
+    return;
+  }
+
+  // Lance le scan via runAllTests
+  const results = await runAllTests(url);
+
+  if (results) {
+    res.status(200).json({result: true, results})
+  }
 });
 
 module.exports = router;
