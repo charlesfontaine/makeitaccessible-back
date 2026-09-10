9ca338bf795677e1b30bee515fc4f658fc3964e7
Charles Fontaine <fontaine.charles64@gmail.com>
2026-09-11 00:56:40 +0200

    refactor: convert connection  to ts

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectionString = process.env.CONNECTION_STRING;
+
+if (!connectionString)
+  throw new Error('CONNECTION_STRING is missing');
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
