24a46643d49fd3030ece02e0d59e0cb36b0abbe5
Charles Fontaine <fontaine.charles64@gmail.com>
2026-09-11 00:54:52 +0200

    config: add types declaration package for supertest and convert app.test to ts script file

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,7 +1,7 @@
-const request = require('supertest');
-const mongoose = require('mongoose');
-const app = require('./app');
-const Audit = require('./models/audits');
+import request from 'supertest';
+import mongoose from 'mongoose';
+import app from './app';
+import Audit from './models/audits';
 
 // Can post an audit as an anonymous user (launch createAuditAction function)
 it('POST /audit', async () => {
